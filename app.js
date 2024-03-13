edc23cff4bb216fc8c684ea4109b761ce5e43293
mYsTeRiOOOOOO <muditarora.kumar@gmail.com>
2024-03-14 00:21:12 +0530

    mudit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path"); // Require the path module
+
+const methodOverride = require("method-override");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {
+    let data = req.body; // Accessing the entire request body
+    console.log(data);
+    res.render("home.ejs",{data}); // Redirecting without passing data in URL
+});
+
+app.get("/thanos",(req,res)=>{
+    res.render("thanosGame.ejs");
+});
+
